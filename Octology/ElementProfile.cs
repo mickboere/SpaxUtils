55038b5a8ff55ff63949f88541555ba138f01f9f
Mick Boere <mickboere@gmail.com>
2024-07-08 20:44:05 +0200

    Vector8 and ElementProfile scripts.

---
+++ Octology/ElementProfile.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace SpaxUtils
+{
+	/// <summary>
+	/// Class that wraps around a <see cref="Vector8"/>, assigning an element to each member.
+	/// Each element has their own characteristics and dynamics with other elements.
+	/// </summary>
+	[Serializable]
+	public class ElementProfile
+	{
+		/// <summary>
+		/// The underlying <see cref="SpaxUtils.Vector8"/> this class wraps around.
+		/// </summary>
+		public Vector8 Vector8;
+
+		/// <summary>
+		/// Lies between Daeth and Light, opposite of Water.
+		/// Masculine head, strong.
+		/// Emotion of Anger, motivation is Action, manifests in Energy.
+		/// </summary>
+		public float Fire { get { return Vector8.N; } set { Vector8.N = value; } }
+
+		/// <summary>
+		/// Lies between Fire and Air, opposite of Nature.
+		/// Masculine tip into Feminine root; smart.
+		/// Emotion of Anticipation, motivation is Continuation, manifests in Current.
+		/// </summary>
+		public float Light { get { return Vector8.NE; } set { Vector8.NE = value; } }
+
+		/// <summary>
+		/// Lies between Light and Daeth, opposite of Earth.
+		/// Feminine base; soft.
+		/// Emotion of Happiness, motivation is Giving, manifests in Ether.
+		/// </summary>
+		public float Air { get { return Vector8.E; } set { Vector8.E = value; } }
+
+		/// <summary>
+		/// Lies between Air and Water, opposite of Daeth.
+		/// Feminine limb; bound.
+		/// Emotion of Acceptance, motivation is Proximity, manifests in Spirit.
+		/// </summary>
+		public float Faeth { get { return Vector8.SE; } set { Vector8.SE = value; } }
+
+		/// <summary>
+		/// Lies between Daeth and Nature, opposite of Fire.
+		/// Feminine head; liquid.
+		/// Emotion of Fear, motivation is Thought, manifests in Flow.
+		/// </summary>
+		public float Water { get { return Vector8.S; } set { Vector8.S = value; } }
+
+		/// <summary>
+		/// Lies between Water and Earth, opposite of Light.
+		/// Feminine tip into Masculine root; growth.
+		/// Emotion of Surprise, motivation is Newness, manifests in Essence.
+		/// </summary>
+		public float Nature { get { return Vector8.SW; } set { Vector8.SW = value; } }
+
+		/// <summary>
+		/// Lies between Nature and Daeth, oppposite of Air.
+		/// Masculine base, solid.
+		/// Emotion of Sadness, motivation is Getting, manifests in Matter.
+		/// </summary>
+		public float Earth { get { return Vector8.W; } set { Vector8.W = value; } }
+
+		/// <summary>
+		/// Lies between Earth and Fire, opposite of Daeth.
+		/// Masculine limb, sharp.
+		/// Emotion of Disgust, motivation is Distance, manifests in Space.
+		/// </summary>
+		public float Daeth { get { return Vector8.NW; } set { Vector8.NW = value; } }
+
+		public ElementProfile(float fire, float light, float air, float faeth, float water, float nature, float earth, float daeth)
+		{
+			this.Fire = fire;
+			this.Light = light;
+			this.Air = air;
+			this.Faeth = faeth;
+			this.Water = water;
+			this.Nature = nature;
+			this.Earth = earth;
+			this.Daeth = daeth;
+		}
+
+		/// <summary>
+		/// Returns a Vector8 with the calculated effectiveness of each element when paired against their opposite element.
+		/// </summary>
+		public Vector8 GetEffectiveness()
+		{
+			return new Vector8(
+				CalculateEffectiveness(Fire, Water),
+				CalculateEffectiveness(Light, Nature),
+				CalculateEffectiveness(Air, Earth),
+				CalculateEffectiveness(Faeth, Daeth),
+				CalculateEffectiveness(Water, Fire),
+				CalculateEffectiveness(Nature, Light),
+				CalculateEffectiveness(Earth, Air),
+				CalculateEffectiveness(Daeth, Faeth));
+		}
+
+		public static float CalculateEffectiveness(float a, float b)
+		{
+			return a / (a + b);
+		}
+
+		public override string ToString()
+		{
+			return $"(Fire={Fire}, Light={Light}, Air={Air}, Faeth={Faeth}, Water={Water}, Nature={Nature}, Earth={Earth}, Daeth={Daeth})";
+		}
+	}
+}
---
+++ Octology/Vector8.cs
@@ -0,0 +1,216 @@
+using System;
+
+namespace SpaxUtils
+{
+	public struct Vector8
+	{
+		public float N, NE, E, SE, S, SW, W, NW;
+
+		public Vector8(float north, float northEast, float east, float southEast, float south, float southWest, float west, float northWest)
+		{
+			N = north;
+			NE = northEast;
+			E = east;
+			SE = southEast;
+			S = south;
+			SW = southWest;
+			W = west;
+			NW = northWest;
+		}
+
+		public Vector8(float[] array)
+		{
+			N = array[0];
+			NE = array[1];
+			E = array[2];
+			SE = array[3];
+			S = array[4];
+			SW = array[5];
+			W = array[6];
+			NW = array[7];
+		}
+
+		/// <summary>
+		/// The sum of all 8 members.
+		/// </summary>
+		public float Sum
+		{
+			get
+			{
+				return N + NE + E + SE + S + SW + W + NW;
+			}
+		}
+
+		#region Arrays
+
+		/// <summary>
+		/// Access the Vector8 members by index, with 0 starting at NORTH, going clockwise.
+		/// </summary>
+		/// <param name="index">The index of the member to access with 0 starting at NORTH, going clockwise.</param>
+		/// <returns>The value of the member corresponding to <paramref name="index"/></returns>.
+		public float this[int index]
+		{
+			get
+			{
+				switch (index)
+				{
+					case 0: return N;
+					case 1: return NE;
+					case 2: return E;
+					case 3: return SE;
+					case 4: return S;
+					case 5: return SW;
+					case 6: return W;
+					case 7: return NW;
+					default:
+						throw new ArgumentOutOfRangeException("index", "Vector8 index needs to be between 0 and 7.");
+				}
+			}
+			set
+			{
+				switch (index)
+				{
+					case 0: N = value; break;
+					case 1: NE = value; break;
+					case 2: E = value; break;
+					case 3: SE = value; break;
+					case 4: S = value; break;
+					case 5: SW = value; break;
+					case 6: W = value; break;
+					case 7: NW = value; break;
+					default:
+						throw new ArgumentOutOfRangeException("index", "Vector8 index needs to be between 0 and 7.");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Convert the Vector8 into an array.
+		/// </summary>
+		/// <returns>A float array of the Vector8's members.</returns>
+		public float[] ToArray()
+		{
+			return new float[8] { N, NE, E, SE, S, SW, W, NW };
+		}
+
+		/// <summary>
+		/// Implicit <see cref="ToArray"/> operator.
+		/// </summary>
+		public static implicit operator float[](Vector8 vector)
+		{
+			return vector.ToArray();
+		}
+
+		#endregion Arrays
+
+		#region Operators
+
+		/// <summary>
+		/// Adds <paramref name="a"/> and <paramref name="b"/> together, resulting in a new <see cref="Vector8"/>.
+		/// </summary>
+		public static Vector8 operator +(Vector8 a, Vector8 b)
+		{
+			return new Vector8(
+				a.N + b.N,
+				a.NE + b.NE,
+				a.E + b.E,
+				a.SE + b.SE,
+				a.S + b.S,
+				a.SW + b.SW,
+				a.W + b.W,
+				a.NW + b.NW);
+		}
+
+		/// <summary>
+		/// Substracts <paramref name="a"/> from <paramref name="b"/>, resulting in a new <see cref="Vector8"/>.
+		/// </summary>
+		public static Vector8 operator -(Vector8 a, Vector8 b)
+		{
+			return new Vector8(
+				a.N - b.N,
+				a.NE - b.NE,
+				a.E - b.E,
+				a.SE - b.SE,
+				a.S - b.S,
+				a.SW - b.SW,
+				a.W - b.W,
+				a.NW - b.NW);
+		}
+
+		/// <summary>
+		/// Multiplies <paramref name="a"/> with <paramref name="b"/>, resulting in a new <see cref="Vector8"/>.
+		/// </summary>
+		public static Vector8 operator *(Vector8 a, Vector8 b)
+		{
+			return new Vector8(
+				a.N * b.N,
+				a.NE * b.NE,
+				a.E * b.E,
+				a.SE * b.SE,
+				a.S * b.S,
+				a.SW * b.SW,
+				a.W * b.W,
+				a.NW * b.NW);
+		}
+
+		/// <summary>
+		/// Divides <paramref name="a"/> by <paramref name="b"/>, resulting in a new <see cref="Vector8"/>.
+		/// </summary>
+		public static Vector8 operator /(Vector8 a, Vector8 b)
+		{
+			return new Vector8(
+				a.N / b.N,
+				a.NE / b.NE,
+				a.E / b.E,
+				a.SE / b.SE,
+				a.S / b.S,
+				a.SW / b.SW,
+				a.W / b.W,
+				a.NW / b.NW);
+		}
+
+		/// <summary>
+		/// Multiplies all members of <paramref name="a"/> by <paramref name="b"/>, resulting in a new <see cref="Vector8"/>.
+		/// </summary>
+		public static Vector8 operator *(Vector8 a, float b)
+		{
+			return new Vector8(
+				a.N * b,
+				a.NE * b,
+				a.E * b,
+				a.SE * b,
+				a.S * b,
+				a.SW * b,
+				a.W * b,
+				a.NW * b);
+		}
+
+		/// <summary>
+		/// Divides all members of <paramref name="a"/> by <paramref name="b"/>, resulting in a new <see cref="Vector8"/>.
+		/// </summary>
+		public static Vector8 operator /(Vector8 a, float b)
+		{
+			return new Vector8(
+				a.N / b,
+				a.NE / b,
+				a.E / b,
+				a.SE / b,
+				a.S / b,
+				a.SW / b,
+				a.W / b,
+				a.NW / b);
+		}
+
+		#endregion Operators
+
+		public override string ToString()
+		{
+			return $"(N={N}, NE={NE}, E={E}, SE={SE}, S={S}, SW={SW}, W={W}, NW={NW})";
+		}
+
+		public string ToStringShort()
+		{
+			return $"({N}, {NE}, {E}, {SE}, {S}, {SW}, {W}, {NW})";
+		}
+	}
+}
