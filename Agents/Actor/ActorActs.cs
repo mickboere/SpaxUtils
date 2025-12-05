897c6b09d8dfc7dbd0fcb8a7493bca5189c29892
Mick Boere <mickboere@gmail.com>
2025-12-05 18:21:57 +0100

    Removed undescriptive "left" / "right" actor acts and replaced with guard / parry

--- Agents/Actor/ActorActs.cs
+++ Agents/Actor/ActorActs.cs
@@ -6,13 +6,11 @@
 
 		public const string TARGET = "Target";
 
-		public const string LEFT = "Left"; // remove
-		public const string RIGHT = "Right"; // remove
+		public const string GUARD = "Guard";
+		public const string PARRY = "Parry";
 
 		public const string LIGHT = "Light";
 		public const string HEAVY = "Heavy";
-		public const string GUARD = "Guard";
-		public const string PARRY = "Parry";
 
 		public const string DASH = "Dash";
 	}
